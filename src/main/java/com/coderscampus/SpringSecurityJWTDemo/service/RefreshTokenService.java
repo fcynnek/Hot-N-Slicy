ff6ca446387da8095054b87ca941745b2f8d4dc1
amamolo9986 <amamolo9986@yahoo.com>
2024-01-14 17:30:32 -0600

    removed unused import in refresh token service

--- src/main/java/com/coderscampus/SpringSecurityJWTDemo/service/RefreshTokenService.java
+++ src/main/java/com/coderscampus/SpringSecurityJWTDemo/service/RefreshTokenService.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
