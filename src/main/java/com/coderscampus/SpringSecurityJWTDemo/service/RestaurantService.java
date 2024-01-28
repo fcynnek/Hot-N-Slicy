dd63adadaab4cca0957fc22dbe74b4f54a59fb7c
amamolo9986 <amamolo9986@yahoo.com>
2024-01-28 15:19:50 -0600

    created restaurant page and id's can be searched for in URL. Next up, the search bar

---
+++ src/main/java/com/coderscampus/SpringSecurityJWTDemo/service/RestaurantService.java
@@ -0,0 +1,26 @@
+package com.coderscampus.SpringSecurityJWTDemo.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.coderscampus.SpringSecurityJWTDemo.domain.Restaurant;
+import com.coderscampus.SpringSecurityJWTDemo.repository.RestaurantRepository;
+
+@Service
+public class RestaurantService {
+	
+	private RestaurantRepository restaurantRepo;
+
+	public RestaurantService(RestaurantRepository restaurantRepo) {
+		super();
+		this.restaurantRepo = restaurantRepo;
+	}
+
+
+	public Restaurant findById(Integer id) {
+		Optional<Restaurant> restaurantOpt = restaurantRepo.findById(id);
+		return restaurantOpt.orElse(null);
+	}
+
+}
--- src/main/java/com/coderscampus/SpringSecurityJWTDemo/web/RestaurantController.java
+++ src/main/java/com/coderscampus/SpringSecurityJWTDemo/web/RestaurantController.java
@@ -1,24 +1,30 @@
 package com.coderscampus.SpringSecurityJWTDemo.web;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.coderscampus.SpringSecurityJWTDemo.domain.Restaurant;
 import com.coderscampus.SpringSecurityJWTDemo.security.FileService;
+import com.coderscampus.SpringSecurityJWTDemo.service.RestaurantService;
 
 @Controller
 public class RestaurantController {
 
 	private FileService fileService;
+	private RestaurantService restaurantService;
 
-	public RestaurantController(FileService fileService) {
+	public RestaurantController(FileService fileService, RestaurantService restaurantService) {
 		super();
 		this.fileService = fileService;
+		this.restaurantService = restaurantService;
 	}
 
-	@GetMapping("/restaurants")
-	public String getSearch(ModelMap model) {
+	@GetMapping("/restaurants/{id}")
+	public String getRestaurantById(@PathVariable Integer id, ModelMap model) {
+		Restaurant restaurant = restaurantService.findById(id);
+		model.put("restaurant", restaurant);
 		return "restaurant";
 	}
 
@@ -26,7 +32,9 @@ public class RestaurantController {
 //	public ResponseEntity<String> processCsv() {
 //		fileService.readFile();
 //		return ResponseEntity.ok("CSV processing successful!");
-//	} this is calling the method that populated the data and saves to db. 
-//  only uncomment when you need to repopulate.	
+//	} 
+//	this is calling the method that populated the data and saves to db. 
+//  only uncomment when you need to repopulate.	Could set up similar
+//  to the AdminController
 	
 }
